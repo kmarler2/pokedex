70fca781fa4ae2273c36ed7216a3f3c62ddd1fc2
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-26 09:59:21 -0600

    add api call

---
+++ src/Utilities/ApiCalls/apiCalls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const pokemonFetch = async () => {
+  try {
+    const url = 'http://localhost:3001/types';
+    const response = await fetch(url);
+    const pokemonData = await response.json();
+    console.log(pokemonData)
+  } catch(error) {
+    console.log(error)
+  }
+}
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import FakeContainer from '../../containers/FakeContainer/'
+import FakeContainer from '../../containers/FakeContainer/';
+import { pokemonFetch } from '../../Utilities/ApiCalls/apiCalls';
+
 
 class App extends Component {
+  constructor() {
+  super();
+    this.state = {
+    } 
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    try {
+    const fetchedData = await pokemonFetch();
+    this.setState({
+      fetchedData
+    })
+  } catch(error) {
+    console.log(error)
+  }
+  } 
 
   render() {
     return (
       <div className='App'>
         <h1 className='header'> POKéDEX </h1>
-        <FakeContainer />
       </div>
     );
   }
 }
+
+export const mapStateToProps = state => ({
+  pokemonData: state.fetchedData
+})
 
 
 export default App;
