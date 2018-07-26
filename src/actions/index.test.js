5822d1853fcd28703ac297e9e2ac3f7338755578
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-26 15:23:10 -0600

    Add action test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,9 +1,10 @@
 import mockData from '../MockData/mockData';
+import {addTypeData, addSelectedType, addPokemonData} from './index.js';
 
 describe('addTypeData', () => {
   it('should have the correct info based on what its passed', () => {
     const pokemon = mockData;
-    const actual = actions.addTypeData(pokemon);
+    const actual = addTypeData(pokemon);
     expect(actual).toEqual({
       type: 'ADD_TYPES', 
       pokemon
@@ -13,12 +14,22 @@ describe('addTypeData', () => {
 
 describe('addSelectedType', () => {
   it('should have the correct info based on what its passed', () => {
-
+    const pokemon = mockData;
+    const actual = addSelectedType(pokemon);
+    expect(actual).toEqual({
+      type: 'ADD_CLICK_TYPE', 
+      pokemon
   })
 })
+  })
 
 describe('addPokemonData', () => {
   it('should have the correct info based on what its passed', () => {
-
+    const pokemon = mockData;
+    const actual = addPokemonData(pokemon);
+    expect(actual).toEqual({
+      type: 'ADD_POKEMON_DATA', 
+      pokemon
   })
 })
+  })
